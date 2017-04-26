dbf9a8a0f9b00cb3aae473daffcc6d2e20cee8dc
Ericteen <chengteen@yeah.net>
2017-04-26 19:58:48 +0800

    new deploy

--- server/routes/dealfn.js
+++ server/routes/dealfn.js
@@ -2,46 +2,45 @@
 let fs = require('fs');
 
 let dealFn = {
-	/**
-	 * [通过Promise写入数据]
-	 * @param  {String} file [file name]
-	 * @param  {Object} obj  [wrote data(object)]
-	 * @return {Object}      [Promise Object]
-	 */
-	writeFileData: (file, obj) => {
-	    let promise = new Promise((resolve, reject) => {
-	        obj = JSON.stringify(obj);
-	        fs.writeFile("./database/" + file, obj, function(err){
-	            if(err) {
-	                reject("fail " + err)
-	            }
-	            else {
-	                resolve("write success!");
-	            }
-	        });
-	    }) 
-	    return promise;
-	},
+    /**
+     * [通过Promise写入数据]
+     * @param  {String} file [file name]
+     * @param  {Object} obj  [wrote data(object)]
+     * @return {Object}      [Promise Object]
+     */
+    writeFileData: (file, obj) => {
+        let promise = new Promise((resolve, reject) => {
+            obj = JSON.stringify(obj);
+            fs.writeFile("./database/" + file, obj, function(err) {
+                if (err) {
+                    reject("fail " + err)
+                } else {
+                    resolve("write success!");
+                }
+            });
+        })
+        return promise;
+    },
 
-	/**
-	 * [the stored data will be loaded by Promise]
-	 * @param  {String} file [file name]
-	 * @return {Object}      [Promise Object]
-	 */
-	readFileData: (file) => {
-	    let promise = new Promise((resolve, reject) => {
-	        fs.readFile("./database/" + file, "utf-8", (err, data) => {
-	            if(err) {
-	                console.log(err);
-	                reject("read filedata error!");
-	            }else {
-	                data = JSON.parse(data);
-	                resolve(data);
-	            }
-	        })
-	    });
-	    return promise;
-	}
+    /**
+     * [the stored data will be loaded by Promise]
+     * @param  {String} file [file name]
+     * @return {Object}      [Promise Object]
+     */
+    readFileData: (file) => {
+        let promise = new Promise((resolve, reject) => {
+            fs.readFile("./database/" + file, "utf-8", (err, data) => {
+                if (err) {
+                    console.log(err);
+                    reject("read filedata error!");
+                } else {
+                    data = JSON.parse(data);
+                    resolve(data);
+                }
+            })
+        });
+        return promise;
+    }
 }
 
 module.exports = dealFn;
